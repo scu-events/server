ac0ed916a32fb2992c5ec383f9d370b37eda3130
KPHarmon <kade_harmon@yahoo.com>
2018-03-10 21:58:57 -0800

    Pulls data from database based on psql call, make to sure add params to const struct, add imports

--- server.go
+++ server.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
+	_ "github.com/lib/pq"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "[]")
-	fmt.Fprintf(w, "Hi Qt ;)")
-}
+const (
+	host     = <ADD HOSTNAME> //probably localhost
+	port     = <ADD PORNUMBER> //probably 5432
+	user     = <ADD USERNAME>
+	password = <ADD PASSWORD>
+	dbname   = <ADD DB NAME>
+)
+
+var (
+	summary  string
+	location string
+)
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+ //Access Server
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo) //Validate credentials
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping() //Open Connection
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query("SELECT summary, location FROM data WHERE location = $1", "Benson") //Add different parameters here based on user input
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close() //Iterate over rows, scanning data we want
+	for rows.Next() {
+		err := rows.Scan(&summary, &location)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(summary, location) //print out the results <-- we can change to put into array
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
