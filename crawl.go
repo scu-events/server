214b3aa910b9f7ae3ef4475ea57af56c88a1f065
MattMistele <matthew@mistele.com>
2019-02-03 16:29:45 -0800

    Update cso calendar link, add Engineering Event calendar

--- crawl.go
+++ crawl.go
@@ -12,12 +12,14 @@ func main() {
 	args := os.Args[1:]
 
 	// TODO: In the future, we should load all the calendars from a text/JSON file online --Matthew
-	//GOOGLE CAL URLS
+	// GOOGLE CAL URLS
 	google_cals := make(map[string]string)
-	//acm club: http://acm.engr.scu.edu/events
+	// acm club: http://acm.engr.scu.edu/events
 	google_cals["acm"] = "https://www.googleapis.com/calendar/v3/calendars/santaclara.acm@gmail.com/events?key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&timeMin=2018-02-24T00:00:00Z&timeMax=2030-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234"
-	//Official CSO/RSO Calendar
-	google_cals["cso"] = "https://clients6.google.com/calendar/v3/calendars/csl@scu.edu/events?calendarId=csl@scu.edu&singleEvents=true&timeZone=America/Los_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2018-02-26T00:00:00-08:00&timeMax=2018-04-02T00:00:00-08:00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
+	// Official CSO/RSO Calendar
+	google_cals["cso"] = "https://www.googleapis.com/calendar/v3/calendars/csl@scu.edu/events?calendarId=csl@scu.edu&key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&&timeMin=2018-02-24T00:00:00Z&timeMax=2099-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234"
+	// Engineering Student Events Calendar - https://www.scu.edu/engineering/beyond-the-classroom/student-organizations/
+	google_cals["engr"] = "https://www.googleapis.com/calendar/v3/calendars/scu.edu_5m30rgamr2hc8ch6e54at49suo@group.calendar.google.com/events?key=AIzaSyCnRyFyPuJ9WSeu602Q7CE13TsxWVNbw10&timeMin=2018-02-24T00:00:00Z&timeMax=2030-04-09T00:00:00Z&singmaxResults=9999&_=1520708172234"
 
 	var wg sync.WaitGroup
 	wg.Add(len(args))
